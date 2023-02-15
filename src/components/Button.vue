a2e79517d7c16318bd276d181cbbf6181bf92d3c
myself <1907312518@qq.com>
2023-02-15 14:53:23 +0800

    创建 Button 组件，并完成样式

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <button class="gulu-button">按钮</button>
+</template>
+
+<style scoped>
+.gulu-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border: 1px solid var(--border-color);
+  border-radius: var(--border-radius);
+  background: var(--button-bg);
+}
+
+.gulu-button:hover {
+  border-color: var(--border-color-hover);
+}
+
+.gulu-button:active {
+  background: var(--button-active-bg);
+}
+
+.gulu-button:focus {
+  outline: none;
+}
+</style>
