fcdbff0472fc8ef8cfb3727eecc98bf4656077aa
myself <1907312518@qq.com>
2023-02-16 22:08:56 +0800

    创建 ButtonGroup 组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import GButton from './components/Button.vue';
+import GButtonGroup from './components/ButtonGroup.vue';
 import {ref} from 'vue';
 
 const loading1 = ref(true);
@@ -11,4 +12,11 @@ const loading3 = ref(false);
   <g-button icon="settings" v-model:loading="loading1">你好</g-button>
   <g-button v-model:loading="loading2">你好</g-button>
   <g-button icon="settings" icon-position="right" v-model:loading="loading3">你好</g-button>
+
+  <g-button-group>
+    <g-button icon="left">上一页</g-button>
+    <g-button>更多</g-button>
+    <g-button icon="right" icon-position="right">下一页</g-button>
+  </g-button-group>
+
 </template>
---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="gulu-button-group">
+    <slot/>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.gulu-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+
+  :deep(.gulu-button) {
+    border-radius: 0;
+    margin-left: -1px;
+
+    &:nth-child(1) {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
