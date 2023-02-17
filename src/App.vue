7612fe705b7ac2a9578af5fcb9d555f95d9bd868
myself <1907312518@qq.com>
2023-02-17 18:24:22 +0800

    创建 Input,支持hover、focus、disabled、readonly并添加样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import GButton from './components/Button.vue';
 import GButtonGroup from './components/ButtonGroup.vue';
+import GInput from './components/Input.vue'
 import {ref} from 'vue';
 
 const loading1 = ref(true);
@@ -9,14 +10,32 @@ const loading3 = ref(false);
 </script>
 
 <template>
-  <g-button icon="settings" v-model:loading="loading1">你好</g-button>
-  <g-button v-model:loading="loading2">你好</g-button>
-  <g-button icon="settings" icon-position="right" v-model:loading="loading3">你好</g-button>
+  <div class="box">
+    <g-button icon="settings" v-model:loading="loading1">你好</g-button>
+    <g-button v-model:loading="loading2">你好</g-button>
+    <g-button icon="settings" icon-position="right" v-model:loading="loading3">你好</g-button>
 
-  <g-button-group>
-    <g-button icon="left">上一页</g-button>
-    <g-button>更多</g-button>
-    <g-button icon="right" icon-position="right">下一页</g-button>
-  </g-button-group>
+    <g-button-group>
+      <g-button icon="left">上一页</g-button>
+      <g-button>更多</g-button>
+      <g-button icon="right" icon-position="right">下一页</g-button>
+    </g-button-group>
+  </div>
+
+  <div class="box">
+    <g-input value="张三"/>
+    <g-input value="李四" disabled/>
+    <g-input value="李四" readonly/>
+    <div>
+      <g-input value="王" error="用户名长度不少于两个字"/>
+    </div>
+  </div>
 
 </template>
+
+<style lang="scss">
+.box{
+  margin: 10px;
+}
+
+</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import GIcon from './Icon.vue';
+
+defineProps({
+  value: String,
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  readonly: {
+    type: Boolean,
+    default: false
+  },
+  error: String
+});
+</script>
+
+<template>
+  <div class="gulu-input-wrapper">
+    <input type="text" :value="value" :disabled="disabled" :readonly="readonly">
+    <template v-if="error">
+      <g-icon name="error" class="gulu-icon-error"/>
+      <span class="gulu-error-message"> {{ error }}</span>
+    </template>
+  </div>
+</template>
+
+<style lang="scss">
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$border-radius: 4px;
+$font-size: 12px;
+$box-shadow-color: rgba(0, 0, 0, .5);
+$red: #f1453d;
+
+.gulu-input {
+  &-wrapper {
+    display: inline-flex;
+    align-items: center;
+    font-size: $font-size;
+
+    :not(:last-child) {
+      margin-right: .5em;
+    }
+
+    > input {
+      font-size: inherit;
+      height: $height;
+      border: 1px solid $border-color;
+      border-radius: $border-radius;
+      padding: 0 8px;
+
+      &:hover {
+        border-color: $border-color-hover;
+      }
+
+      &:focus {
+        outline: none;
+        box-shadow: inset 0 1px 3px $box-shadow-color;
+      }
+
+      &[disabled], &[readonly] {
+        border-color: #bbb;
+        color: #bbb;
+        cursor: not-allowed;
+      }
+    }
+
+    > .gulu-icon-error {
+      fill: $red;
+    }
+
+    > .gulu-error-message {
+      color: $red;
+    }
+  }
+
+}
+</style>
