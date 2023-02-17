1e119a1e83b562656dc774c31d37efc4d9b5b26a
myself <1907312518@qq.com>
2023-02-17 18:31:44 +0800

    添加error时，input样式

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -17,7 +17,7 @@ defineProps({
 
 <template>
   <div class="gulu-input-wrapper">
-    <input type="text" :value="value" :disabled="disabled" :readonly="readonly">
+    <input type="text" :class="{'gulu-input-error':error}" :value="value" :disabled="disabled" :readonly="readonly">
     <template v-if="error">
       <g-icon name="error" class="gulu-icon-error"/>
       <span class="gulu-error-message"> {{ error }}</span>
@@ -34,47 +34,48 @@ $font-size: 12px;
 $box-shadow-color: rgba(0, 0, 0, .5);
 $red: #f1453d;
 
-.gulu-input {
-  &-wrapper {
-    display: inline-flex;
-    align-items: center;
-    font-size: $font-size;
+.gulu-input-wrapper {
+  display: inline-flex;
+  align-items: center;
+  font-size: $font-size;
 
-    :not(:last-child) {
-      margin-right: .5em;
+  :not(:last-child) {
+    margin-right: .5em;
+  }
+
+  > input {
+    font-size: inherit;
+    height: $height;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    padding: 0 8px;
+
+    &:hover {
+      border-color: $border-color-hover;
     }
 
-    > input {
-      font-size: inherit;
-      height: $height;
-      border: 1px solid $border-color;
-      border-radius: $border-radius;
-      padding: 0 8px;
-
-      &:hover {
-        border-color: $border-color-hover;
-      }
-
-      &:focus {
-        outline: none;
-        box-shadow: inset 0 1px 3px $box-shadow-color;
-      }
-
-      &[disabled], &[readonly] {
-        border-color: #bbb;
-        color: #bbb;
-        cursor: not-allowed;
-      }
+    &:focus {
+      outline: none;
+      box-shadow: inset 0 1px 3px $box-shadow-color;
     }
 
-    > .gulu-icon-error {
-      fill: $red;
-    }
-
-    > .gulu-error-message {
-      color: $red;
+    &[disabled], &[readonly] {
+      border-color: #bbb;
+      color: #bbb;
+      cursor: not-allowed;
     }
   }
 
+  > .gulu-icon-error {
+    fill: $red;
+  }
+
+  > .gulu-error-message {
+    color: $red;
+  }
+
+  > .gulu-input-error {
+    border-color: $red;
+  }
 }
 </style>
